b7693ad09eb5cba4ac9b486b11c9c11574d78890
Christopher David <chris@arcade.city>
2021-12-18 16:18:37 -0600

    reset root storage key

--- lib/mst.ts
+++ lib/mst.ts
@@ -17,7 +17,7 @@ export const EventModel = types.model('Event').props({
 
 export interface Event extends Instance<typeof EventModel> {}
 
-export const ROOT_STATE_STORAGE_KEY = 'root9'
+export const ROOT_STATE_STORAGE_KEY = 'root10'
 
 export const RootStoreModel = types
   .model({
