b5cf845d4682ad6946f7f806aa9d42f1bce44e22
Christopher David <chris@arcade.city>
2021-12-18 16:11:14 -0600

    fix mobile logout

--- components/mvp/Navbar.tsx
+++ components/mvp/Navbar.tsx
@@ -59,6 +59,7 @@ function Navbar() {
                 <button
                   type='button'
                   className='ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+                  onClick={handleLogout}
                 >
                   <span className='sr-only'>View notifications</span>
                   <LogoutIcon className='h-6 w-6' aria-hidden='true' />
